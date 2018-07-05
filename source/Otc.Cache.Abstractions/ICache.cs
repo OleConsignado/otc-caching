ea7cfe3680da86d98e6fd06bd02c259062359df6
bsi90586 <bsi90586@LOC3768>
2018-07-05 11:27:02 -0300

    incluindo a solução

---
+++ source/Otc.Cache.Abstractions/ICache.cs
@@ -0,0 +1,11 @@
+﻿namespace Otc.Cache.Abstractions
+{
+    public interface ICache
+    {
+        void Set<T>(string key, T entity, int minutes) where T : class;
+
+        bool TryGetValue<T>(string key, out T entity) where T : class;
+
+        void Remove(string key);
+    }
+}
