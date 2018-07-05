5591e13aa63e4dff52825df6e4619d0b125c4a42
Luciano Lima <37223380+lucianolimao@users.noreply.github.com>
2018-07-05 13:54:21 -0300

    criação do cicd

---
+++ cicd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+CICD_COMMON_VERSION="v2.2"
+
+export CLASS_LIBRARY_PROJ_DIR=Source/Otc.Cache.Abstractions
+export TEST_PROJ_DIR=Source/Otc.Cache.Tests
+
+cd $TRAVIS_BUILD_DIR
+
+wget -q https://raw.githubusercontent.com/OleConsignado/otc-cicd-common/$CICD_COMMON_VERSION/cicd-common.sh -O ./cicd-common.sh
+chmod +x ./cicd-common.sh
+
+./cicd-common.sh $@
+
+export CLASS_LIBRARY_PROJ_DIR=Source/Otc.Cache
+
+./cicd-common.sh $@
